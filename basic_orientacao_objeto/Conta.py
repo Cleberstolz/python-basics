9efa8ede745476103915e30c4febd69e3079de1e
Cleber Stolz <cleberst_stolz@hotmail.com>
2022-06-04 22:27:24 -0300

    basic conta e cliente

--- basic_orientacao_objeto/Conta.py
+++ basic_orientacao_objeto/Conta.py
@@ -18,3 +18,19 @@ class Conta:
     def tranferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
